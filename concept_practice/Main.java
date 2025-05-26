ff3f444e2786976b65d620d058bda77b15937e85
Arup53 <arupkumarhowlader12@gmail.com>
2025-05-26 10:59:07 +0600

    complete task-2

--- concept_practice/Main.java
+++ concept_practice/Main.java
@@ -1,16 +1,25 @@
+import java.util.ArrayList;
+
 public class Main {
+
+    public static void printPersons(ArrayList<Person> persons){
+        for(Person person:persons){
+            System.out.println(person);
+        }
+    }
+
+
     public static void main(String[] args) {
-        A a= new A();
-        B b= new B();
-        C c= new C();
 
-        Superclass sup = new Superclass();
-        Subclass sub = new Subclass();
 
-        System.out.println(sup);
-        System.out.println(sub);
+        Person ada= new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN",1200);
+        Person esko= new Teacher("Esko Ukkonen", "Mannerheimintie 15 00100 Helsinki",5600);
 
-     
+        ArrayList<Person> persons = new ArrayList<Person>();
+        persons.add(new Teacher("Ada Lovelace", "24 Maddox St. London W1S 2QN", 1200));
+        persons.add(new Student("Ollie", "6381 Hollywood Blvd. Los Angeles 90028"));
+
+        printPersons(persons);
     }
 }
 
@@ -59,9 +68,60 @@ class Subclass extends Superclass {
 
     @Override
     public String toString() {
-       return super.toString() + "\n  And let's add my own message to it!";
+       return  "\n  And let's add my own message to it!";
     }
 }
 
 
 // ------- Task-2 --------
+class Person{
+    private String name;
+    private String address;
+
+    public Person(String name, String address){
+        this.name=name ;
+        this.address= address;
+    }
+
+    public String toString(){
+        return this.name+"\n"+" "+this.address ;
+    }
+}
+
+class Student extends Person{
+    private int creditScore;
+
+    // in super method this.name, this.address  will not work as the object is not created then how will its address be found
+    public Student(String name, String address){
+        super(name, address);
+        this.creditScore=0;
+    }
+
+    public int credit(){
+        return this.creditScore ;
+    }
+
+    public void study(){
+        this.creditScore++ ;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString()+"\n"+"Study credits"+" "+this.creditScore;
+    }
+}
+
+
+class Teacher extends Person{
+    private int salary;
+
+    public Teacher(String name,String address, int salary){
+        super(name,address);
+        this.salary=salary;
+    }
+
+    @Override
+    public String toString(){
+        return super.toString()+"\n"+"Salary"+" "+this.salary+" "+"euro/month" ;
+    }
+}
