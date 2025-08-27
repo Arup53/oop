17ebeb2feacac3f20f42455fe9e1e2b8469c940d
Arup53 <arupkumarhowlader12@gmail.com>
2025-08-27 09:47:13 +0600

    create schedule class

--- Projects/UniversityManagement/src/models/Schedule.java
+++ Projects/UniversityManagement/src/models/Schedule.java
@@ -1,3 +1,14 @@
 public class Schedule {
-    
+    private String dayOfWeek ;
+    private String startTime ;
+    private String endTime ;
+    private String room ;
+
+
+    public Schedule(String dayOfWeek,String startTime,String endTime,String room ){
+        this.dayOfWeek= dayOfWeek ;
+        this.startTime= startTime ;
+        this.endTime =endTime ;
+        this.room = room ;
+    }
 }
