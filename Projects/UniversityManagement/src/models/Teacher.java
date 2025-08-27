3c0b325b7e8ddb54fa19560b1ce30ee2bfbb3f37
Arup53 <arupkumarhowlader12@gmail.com>
2025-08-27 09:38:59 +0600

    add 2 methods in Teacher class

--- Projects/UniversityManagement/src/models/Teacher.java
+++ Projects/UniversityManagement/src/models/Teacher.java
@@ -1,10 +1,11 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class Teacher extends Person {
     private String employeeId ;
     private String department ;
     private String rank ;
-    // private List<Course> coursesTaught;
+    private List<Course> coursesTaught = new ArrayList<>() ;
     
     public Teacher(String employeeId, String department, String rank){
         this.employeeId= employeeId ;
@@ -17,6 +18,11 @@ public class Teacher extends Person {
         return "Teacher";
     }}
 
-
+    public void assignCourse(Course course){
+        coursesTaught.add(course);
+    }
+    public void removeCourse(Course course){
+        coursesTaught.remove(course);
+    }
 
 }
