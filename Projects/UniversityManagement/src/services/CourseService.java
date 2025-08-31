a105f79e28fa56f226b2b7c7e60a6d16af7f42f0
Arup53 <arupkumarhowlader12@gmail.com>
2025-08-31 10:01:54 +0600

    add courseservice and implement constructor

---
+++ Projects/UniversityManagement/src/services/CourseService.java
@@ -0,0 +1,19 @@
+package services;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import models.Course;
+
+public class CourseService {
+    private List<Course> courses ;
+    private Map<String, Course> courseIndex ;
+
+    public CourseService(){
+        this.courses= new ArrayList<>() ;
+        this.courseIndex = new HashMap<>();
+    }
+
+}
