93ccd7413c09be35e19cf13bf7b2f1aeb82d3c26
Arup53 <arupkumarhowlader12@gmail.com>
2025-09-02 10:05:24 +0600

    create enrollmentservice class and implement enrollStudentInCourse method

---
+++ Projects/UniversityManagement/src/services/EnrollmentService.java
@@ -0,0 +1,27 @@
+package services;
+
+import models.Course;
+import models.Student;
+
+public class EnrollmentService {
+    private StudentService studentService ;
+    private CourseService courseService ;
+
+    public EnrollmentService(StudentService studentService, CourseService courseService){
+        this.studentService= studentService ;
+        this.courseService = courseService ;
+    }
+
+    public boolean enrollStudentInCourse(String studentId, String courseId){
+        if(studentId == null || studentId.trim().isEmpty() || courseId == null || courseId.trim().isEmpty()){
+            throw new IllegalArgumentException("studentid or courseId can not be null");
+        }
+        Student student = studentService.findStudentById(studentId);
+        Course  course  = courseService.findCourseById(courseId);
+
+        student.addCourse(course);
+        course.addStudent(student);
+
+        return true ;
+    }
+}
