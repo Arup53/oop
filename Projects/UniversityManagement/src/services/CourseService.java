e4c60f51133ce09aa3624c683d64f3f0e28a9c2e
Arup53 <arupkumarhowlader12@gmail.com>
2025-09-02 09:33:58 +0600

    Implement AddCourse method in courseserivce class

--- Projects/UniversityManagement/src/services/CourseService.java
+++ Projects/UniversityManagement/src/services/CourseService.java
@@ -16,6 +16,19 @@ public class CourseService {
         this.courseIndex = new HashMap<>();
     }
 
-   
+    // implement crud on courses model with valiadation ; a middle layer for calling the methods of course  class
+    public boolean AddCourse(Course course){
+        if(course == null || course.getCourseId()== null) {
+            throw new IllegalArgumentException("Course and course id can't be null");
+        }
+
+        if(courseIndex.containsKey(course.getCourseId())){
+            return false ;
+        }
+
+        courses.add(course);
+        courseIndex.put(course.getCourseId(), course);
+        return true ;
+    }
 
 }
